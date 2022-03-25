97bf039133b47e201c4e7849f884325e8abc44ef
bdorz <39293574+bdorz@users.noreply.github.com>
2022-03-25 11:16:25 +0800

    Delete TEst6.cs

--- TEst6.cs
+++
@@ -1,9 +0,0 @@
-public class Program
-{
-    public static void Main()
-    {
-        System.Console.WriteLine("Hello, world!");
-        System.Console.WriteLine("Press any key to exit..");
-        System.Console.Read();
-    }
-}

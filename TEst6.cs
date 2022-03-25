e75563f9eb0ee00da3bfe90b8ab4086e2d4c2e70
bdorz <39293574+bdorz@users.noreply.github.com>
2022-03-25 11:11:06 +0800

    Add files via upload

---
+++ TEst6.cs
@@ -0,0 +1,9 @@
+public class Program
+{
+    public static void Main()
+    {
+        System.Console.WriteLine("Hello, world!");
+        System.Console.WriteLine("Press any key to exit..");
+        System.Console.Read();
+    }
+}
